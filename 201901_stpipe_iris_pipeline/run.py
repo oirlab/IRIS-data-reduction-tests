50297811fa415454bc33ec7bf844e6898b660503
Andrea Zonca <code@andreazonca.com>
2019-02-03 22:20:22 -0800

    moved environment definition to bash script

--- 201901_stpipe_iris_pipeline/run.py
+++ 201901_stpipe_iris_pipeline/run.py
@@ -1,7 +1,4 @@
 import os
-os.environ["CRDS_PATH"]="crds_cache"
-os.environ["CRDS_CONTEXT"]="tmt_0001.pmap"
-os.environ["CRDS_SERVER_URL"]="https://crds-serverless-mode.stsci.edu"
 
 from jwst.pipeline import Image2Pipeline
 from iris_pipeline.pipeline import Image2Pipeline
